7118b15d29b76b0bfb961184ef7b2ea8a25c94b8
Palden sherpa <spalden311@gmail.com>
2025-02-13 09:20:59 +0545

    My first commit

---
+++ project/about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Travel Website</title>
+</head>
+<body>  
+<<?php 
+  include 'menu.php';
+?>
+
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-5"> GT Traveller </h1>
+    <p class="lead"> Join with us & travel the world with the new vision  </p>
+  </div>
+</div>
+
+<section class="my-1">
+    <center><div class="py-5">
+        <h2>About Us</h2>
+    </div></center>
+
+    <div class="container fluid">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+            <img src="img/22.jpg" class="img-fluid aboutimg" >
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-12">
+        <h3><b class="display-4"> Anthony Bourdain </b></h3>
+            <p class="">“Travel isn’t always pretty. It isn’t always comfortable. Sometimes it hurts, it even breaks your heart. But that’s okay. The journey changes you; it should change you. It leaves marks on your memory, on your consciousness, on your heart, and on your body. You take something with you. Hopefully, you leave something good behind.”</p>
+        <a href="about.php" class="btn btn-success">More About Us</a>
+        </div>
+
+    </div>
+    </div>
+
+</section>
+<center><h3><p class="pd-5">Thank You For Visiting Page</p></h3></center>
+
+<footer>
+    <p class="p-3 bg-dark text-white text-center"> @GT Traveller</p>
+</footer>
+</body>
+</html>
---
+++ project/contact.php
@@ -0,0 +1,21 @@
+<?php 
+  include 'menu.php';
+?>
+
+<section class="my-1">
+    <center><div class="py-5">
+        <h2>Contact</h2>
+    </div></center>
+</section>
+
+
+<center><b><u><h2>GT Traveller</h2></u></b>
+<b><h3>Address</h3></b>
+<p>Near Gurudeo Square</br>
+Netaji Square Babupeth</br>
+Chandrapur 442401</p>
+<b><p>Mobile Number :  9765538461</p>
+<p>Email Id      : gt@gmail.com</p></b>
+</center>
+
+?>
